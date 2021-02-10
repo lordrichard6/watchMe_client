4f48e80c2996604eca307098466889e73f988996
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-10 14:43:22 +0100

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// react-router -------- core navigation lib, we don't install this manually
+// react-router-dom ---- navigation for dom-based apps (for this app)
+// react-router-native - navigation for react-native apps
+// react-router-redux -- bindings between redux and react router (not necessary)
